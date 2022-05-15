9b041d1e8806310359066ecde1b555fae682c618
NickFikatas <fikatas.nick@gmail.com>
2022-05-15 19:00:15 +0300

    Changes

--- js/main.js
+++ js/main.js
@@ -24,21 +24,86 @@ function OnClick(){
 searchBar.addEventListener("keyup", OnKeyUp);
 clearIcon.addEventListener("click", OnClick);
 //--------------------------------------------------------------------------
-
+/* CONTROLS THE ZOOM IN - OUT OF THE IMAGES OF THE CATEGORIES WHEN MOUSE IS HOVERING OVER*/
 const categoryLi = document.querySelector(".category li");
 const categoryLiImg = document.querySelector(".category li img");
 
-function ScaleImage(){
-   categoryLiImg.style.transform = 'scale(1.07)';
-   categoryLiImg.style.transitionDuration  = "0.2s";
-}
+const house_garden = document.querySelector(".house_garden");
+const house_gardenIMG = document.getElementById("house_garden");
 
-function UnscaleImage(){
+const fashion = document.querySelector(".fashion");
+const fashionIMG = document.getElementById("fashion");
+
+const sports = document.querySelector(".sports");
+const sportsIMG = document.getElementById("sportsIMG");
+
+const pets = document.querySelector(".pets");
+const petsIMG = document.getElementById("petsIMG");
+
+
+function ScalecategoryLiImg(){
+  categoryLiImg.style.transform = 'scale(1.07)';
+  categoryLiImg.style.transitionDuration  = "0.2s";
+
+}
+function UnscalecategoryLiImg(){
   categoryLiImg.style.transform = 'scale(1)';
   categoryLiImg.style.transitionDuration  = "0.2s";
 }
+
+function ScaleHouse_Garden(){
+  house_gardenIMG.style.transform = 'scale(1.07)';
+  house_gardenIMG.style.transitionDuration  = "0.2s";
+}
+
+function UnscaleHouse_Garden(){
+  house_gardenIMG.style.transform = 'scale(1)';
+  house_gardenIMG.style.transitionDuration  = "0.2s";
+}
+
+function ScaleFashion(){
+  fashionIMG.style.transform = 'scale(1.07)';
+  fashionIMG.style.transitionDuration  = "0.2s";
+}
+
+function UnscaleFashion(){
+  fashionIMG.style.transform = 'scale(1)';
+  fashionIMG.style.transitionDuration  = "0.2s";
+}
+
+function ScaleSports(){
+  sportsIMG.style.transform = 'scale(1.07)';
+  sportsIMG.style.transitionDuration  = "0.2s";
+}
+
+function UnscaleSports(){
+  sportsIMG.style.transform = 'scale(1)';
+  sportsIMG.style.transitionDuration  = "0.2s";
+}
+
+function ScalePets(){
+  petsIMG.style.transform = 'scale(1.07)';
+  petsIMG.style.transitionDuration  = "0.2s";
+}
+
+function UnscalePets(){
+  petsIMG.style.transform = 'scale(1)';
+  petsIMG.style.transitionDuration  = "0.2s";
+}
 //When mouse hovers element
-categoryLi.onmouseover = ScaleImage;
+categoryLi.onmouseover = ScalecategoryLiImg;
 //When mouse stops hovering over element
-categoryLi.onmouseleave = UnscaleImage;
+categoryLi.onmouseleave = UnscalecategoryLiImg;
+
+house_garden.onmouseover = ScaleHouse_Garden;
+house_garden.onmouseleave = UnscaleHouse_Garden;
+
+fashion.onmouseover = ScaleFashion;
+fashion.onmouseleave = UnscaleFashion;
+
+pets.onmouseover = ScalePets;
+pets.onmouseleave = UnscalePets;
+
+sports.onmouseover = ScaleSports;
+sports.onmouseleave = UnscaleSports;
 //---------------------------------------------------------------------------
